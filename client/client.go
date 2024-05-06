e4742a6012b2323a94ec0ad1696f050dc9eef9c9
KeYang <kyang72@my.bcit.ca>
2024-05-06 10:54:46 -0700

    testing on cgo with C file

--- client/client.go
+++ client/client.go
@@ -1,99 +1,114 @@
 package main
 
+/*
+#cgo CFLAGS: -I/opt/homebrew/opt/openssl@3/include
+#cgo LDFLAGS: -L/opt/homebrew/opt/openssl@3/lib -lcrypto -L. -lencryption
+#include "encryption.h"
+*/
 import (
-	"bufio"
+	// "bufio"
 	"fmt"
-	"net"
+	// "net"
+	"C"
 )
 
-type ClientState int
+// type ClientState int
 
-const(
-	Init ClientState = iota
-	KeyGeneration
-	EstablishConnection
-	KeyExchange
-	GenerateSharedSecret
-	GetUserInput
-	Encryption
-	SendString
-	FatalError
-	Termination
-)
+// const(
+// 	Init ClientState = iota
+// 	KeyGeneration
+// 	EstablishConnection
+// 	KeyExchange
+// 	GenerateSharedSecret
+// 	GetUserInput
+// 	Encryption
+// 	SendString
+// 	FatalError
+// 	Termination
+// )
 
-type ClientFSM struct {
-	currentState ClientState
-	err error
-	ip net.IP
-	port int
-	conn net.TCPConn 
-	writer *bufio.ReadWriter
-	privateKey string
-	publicKey string
-	shareSecret string
-	inputString string
-}
+// type ClientFSM struct {
+// 	currentState ClientState
+// 	err error
+// 	ip net.IP
+// 	port int
+// 	conn net.TCPConn
+// 	writer *bufio.ReadWriter
+// 	privateKey string
+// 	publicKey string
+// 	shareSecret string
+// 	inputString string
+// }
 
-func(fsm *ClientFSM) init_state() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) init_state() ClientState {
+// 	return 1
+// }
 
-func(fsm *ClientFSM) key_generation_state() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) key_generation_state() ClientState {
+// 	return 1
+// }
 
-func(fsm *ClientFSM) establish_connection_state() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) establish_connection_state() ClientState {
+// 	return 1
+// }
 
-func(fsm *ClientFSM) key_exchange_state() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) key_exchange_state() ClientState {
+// 	return 1
+// }
 
-func(fsm *ClientFSM) generate_shared_secret_state() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) generate_shared_secret_state() ClientState {
+// 	return 1
+// }
 
-func(fsm *ClientFSM) get_user_input() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) get_user_input() ClientState {
+// 	return 1
+// }
 
-func(fsm *ClientFSM) encryption_state() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) encryption_state() ClientState {
+// 	return 1
+// }
 
-func(fsm *ClientFSM) fatal_error_state() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) fatal_error_state() ClientState {
+// 	return 1
+// }
 
-func(fsm *ClientFSM) termination_state() ClientState {
-	return 1
-}
+// func(fsm *ClientFSM) termination_state() ClientState {
+// 	return 1
+// }
 
+// func (fsm *ClientFSM) Run() {
+// 	for {
+// 		switch fsm.currentState {
+// 			case Init:
+// 				fsm.currentState = fsm.init_state()
+// 			case KeyGeneration:
+// 				fsm.currentState = fsm.key_generation_state()
+// 			case EstablishConnection:
+// 				fsm.currentState = fsm.establish_connection_state()
+// 			case KeyExchange:
+// 				fsm.currentState = fsm.key_exchange_state()
+// 			case GenerateSharedSecret:
+// 				fsm.currentState = fsm.generate_shared_secret_state()
+// 			case GetUserInput:
+// 				fsm.currentState = fsm.get_user_input()
+// 			case Encryption:
+// 				fsm.currentState = fsm.encryption_state()
+// 			case FatalError:
+// 				fsm.currentState = fsm.fatal_error_state()
+// 			case Termination:
+// 				fsm.termination_state()
+// 				return
+// 		}
+// 	}
+// }
 
-
-func (fsm *ClientFSM) Run() {
-	for {
-		switch fsm.currentState {
-			case Init:
-				fsm.currentState = fsm.init_state()
-			case KeyGeneration:
-				fsm.currentState = fsm.key_generation_state()
-			case EstablishConnection:
-				fsm.currentState = fsm.establish_connection_state()
-			case KeyExchange:
-				fsm.currentState = fsm.key_exchange_state()
-			case GenerateSharedSecret:
-				fsm.currentState = fsm.generate_shared_secret_state()
-			case GetUserInput:
-				fsm.currentState = fsm.get_user_input()
-			case Encryption:
-				fsm.currentState = fsm.encryption_state()
-			case FatalError:
-				fsm.currentState = fsm.fatal_error_state()
-			case Termination:
-				fsm.termination_state()
-				return
-		}
+func main() {
+	key := C.create_key()
+	if key == nil {
+		fmt.Println("Failed to create key")
+		return
 	}
+	defer C.EC_KEY_free(key)
+
+	fmt.Println("Key created successfully")
 }
---
+++ client/encryption.h
@@ -0,0 +1,14 @@
+#ifndef ENCRYPTION_H
+#define ENCRYPTION_H
+
+#include <openssl/ec.h>
+#include <openssl/err.h>
+#include <openssl/obj_mac.h>
+#include <openssl/ecdh.h>
+#include <openssl/evp.h>
+
+EC_KEY *create_key(void);
+unsigned char *get_secret(EC_KEY *key, const EC_POINT *peer_pub_key,
+			size_t *secret_len);
+
+#endif // ENCRYPTION_H
